0e8366ff203394cefaec2e089c2fd6fa25f12233
igaozp <igaozp@hotmail.com>
2020-07-22 22:15:44 +0800

    Feat: Add jwt auth strategy

--- build-api-with-nestjs/src/modules/auth/auth.service.ts
+++ build-api-with-nestjs/src/modules/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UsersService) {}
+    constructor(
+        private readonly userService: UsersService,
+        private readonly jwtService: JwtService,
+    ) { }
 
     async validateUser(username: string, password: string) {
         const user = await this.userService.findOneByEmail(username);
@@ -21,8 +26,35 @@ export class AuthService {
         return result;
     }
 
+    private async login(user: User) {
+        const token = await this.generateToken(user);
+        return { user, token };
+    }
+
+    private async create(user: User) {
+        const password = await this.hashPassword(user.password);
+
+        const newUser = await this.userService.create({ ...user, password: password });
+
+        const { pass, ...result } = newUser['dataValues'];
+
+        const token = await this.generateToken(result);
+
+        return { user: result, token };
+    }
+
     private async comparePassword(enteredPassword: string, dbPassword: string) {
         const match = await bcrypt.compare(enteredPassword, dbPassword);
         return match;
+    }
+
+    private async generateToken(user: User) {
+        const token = await this.jwtService.signAsync(user);
+        return token;
+    }
+
+    private async hashPassword(password: string) {
+        const hash = await bcrypt.hash(password, 10);
+        return hash;
     }
 }
